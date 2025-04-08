6753ebb137eaad0b9aeebe9715cad78dc305e58a
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-04-09 01:18:02 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GOOGLE_CLIENT_Id } from '../../Urls.js'
+import { GOOGLE_CLIENT_Id } from '../Urls.js'
 // import { ChakraProvider} from '@chakra-ui/react'
 import "../src/styles/main.scss";
 
