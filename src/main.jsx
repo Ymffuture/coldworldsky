917e62506cd03c46eb964dc79562e1a0d724dc5c
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-04-09 01:13:52 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import {Google} from '../../Urls.js'
 // import { ChakraProvider} from '@chakra-ui/react'
 import "../src/styles/main.scss";
-const _clientId = process.env.GOOGLE_CLIENT_Id
+
 
 createRoot(document.getElementById('root')).render(
   
 
- <GoogleOAuthProvider clientId={_clientId} >
+ <GoogleOAuthProvider clientId={} >
 
    <StrictMode>
     <App />
