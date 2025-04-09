0a1f3ca37068ccb9574d3c35be2fe760848994bd
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-04-10 01:54:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -137,7 +137,7 @@ useEffect(()=>{
           <Route path="/tutoring/ApplicationForm-for-a-tutor/" element={<TutorApplyForm/>} >
           <Route path="/tutoring/ApplicationForm-for-a-tutor/track-Application/" element={<TrackApplication/>} />
           </Route>
-            <Route path="/tutoring/subjects" element={<Subjects/>}>
+            <Route path="/tutoring/subjects" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Subjects/></ProtectedRoute>}>
               <Route path="/tutoring/subjects/Life-sciences" element={<LifeSciences />} >
                 <Route path="/tutoring/subjects/Life-sciences/clip-overview" element={<VidLfs />} />
               </Route>
@@ -152,7 +152,7 @@ useEffect(()=>{
           </Route>
 
 
-  <Route path="/find-a-tutor" element={<FindTutor />} />
+  <Route path="/find-a-tutor" element={<ProtectedRoute isAuthenticated={isAuthenticated}><FindTutor/></ProtectedRoute>} />
 
 
           <Route path="/courses/" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Courses/></ProtectedRoute>}>
