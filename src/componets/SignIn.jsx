b8aa0683c1613a93ae96f65582aac6034e76ae8e
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-04-10 03:05:13 +0200

    Update SignIn.jsx

--- src/componets/SignIn.jsx
+++ src/componets/SignIn.jsx
@@ -72,7 +72,10 @@ const SignIn = ({ setIsAuthenticated }) => {
       // Save token to localStorage
       localStorage.setItem("token", data.token);
       setIsAuthenticated(true); 
-      navigate("/");
+      navigate("/courses");
+      setTimeout(() =>{
+        window.location.reload()
+       } ,3000) 
     } else {
       
       toast.error(data.error || "No internet Connection", {
