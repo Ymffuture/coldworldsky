85ff75bd5e140be3d4ccf4afb1416cc9b7b55d47
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-04-13 04:35:58 +0200

    Update image.jsx

--- src/componets/image.jsx
+++ src/componets/image.jsx
@@ -4,24 +4,23 @@ import { Carousel, Container } from 'react-bootstrap';
 function ShapeExample() {
   return (
     <Container className="py-5">
-      <Carousel fade interval={3000} controls={false} indicators={false}>
+      <Carousel fade controls={false} indicators={false} interval={4000}>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="../img/unisa.jpg"
+            src="../img/unisa.jpg" // Use your image path here
             alt="UNISA"
-            style={{ height: "300px", objectFit: "cover", borderRadius: "10px" }}
+            style={{
+              height: "350px",
+              objectFit: "cover",
+              borderRadius: "20px",
+              boxShadow: "0 8px 20px rgba(0,0,0,0.15)"
+            }}
           />
+          <Carousel.Caption>
+            <h5 className="bg-dark bg-opacity-50 p-2 rounded">University of South Africa (UNISA)</h5>
+          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="../img/uj-1.jpg"
-            alt="UJ"
-            style={{ height: "300px", objectFit: "cover", borderRadius: "50%" }}
-          />
-        </Carousel.Item>
-        {/* Add more slides as needed */}
       </Carousel>
     </Container>
   );
