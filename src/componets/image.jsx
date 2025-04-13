331c5495d2199e0d31dbc0701620b8f2311cfa58
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-04-13 04:39:29 +0200

    Update image.jsx

--- src/componets/image.jsx
+++ src/componets/image.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import { Carousel, Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function ShapeExample() {
   return (
     <Container className="py-5">
-      <Carousel fade controls={false} indicators={false} interval={4000}>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="../img/unisa.jpg" // Use your image path here
-            alt="UNISA"
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <div
             style={{
+              backgroundImage: "url('../img/unisa.jpg')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
               height: "350px",
-              objectFit: "cover",
               borderRadius: "20px",
-              boxShadow: "0 8px 20px rgba(0,0,0,0.15)"
+              boxShadow: "0 8px 24px rgba(0,0,0,0.2)",
             }}
+            role="img"
+            aria-label="University of South Africa (UNISA)"
           />
-          <Carousel.Caption>
-            <h5 className="bg-dark bg-opacity-50 p-2 rounded">University of South Africa (UNISA)</h5>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+        </Col>
+      </Row>
     </Container>
   );
 }
