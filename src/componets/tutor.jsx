8207ba3bc7b27d798c284109dfff1af06aa1fa2f
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-04-13 04:22:24 +0200

    Update tutor.jsx

--- src/componets/tutor.jsx
+++ src/componets/tutor.jsx
@@ -19,16 +19,6 @@ const Tutor = (props) => {
 
   const fade = useSpring({ opacity: isTransitioning ? 0 : 1, transform: isTransitioning ? "translateY(20px)" : "translateY(0)" });
 	
-useEffect(() => {
-  const script = document.createElement("script");
-  script.type = "application/ld+json";
-  script.text = JSON.stringify(faqSchema);
-  document.head.appendChild(script);
-
-  return () => {
-    document.head.removeChild(script); // Cleanup on unmount
-  };
-}, []);
 
   const faqSchema = {
     "@context": "https://schema.org",
