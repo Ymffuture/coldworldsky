0dcbd520e1f776baedf79f6a9ba19b223c9d1442
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-04-22 01:46:34 +0200

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,31 +8,34 @@ import Team from '../componets/Team';
 import JsonData from '../data/data.json';
 import SmoothScroll from "smooth-scroll";
 import SideButton from "./SideButton";
+import AnimatedSection from "../componets/AnimatedSection"; // Import the wrapper
+
 export const scroll = new SmoothScroll('a[href*="/"]', {
   speed: 500,
   speedAsDuration: true,
 });
+
 const LandingPage = () => {
   const [landingPageData, setLandingPageData] = useState({});
+  
   useEffect(() => {
-
-    setTimeout(()=>{
+    setTimeout(() => {
       setLandingPageData(JsonData);
-    },5000)
-    
+    }, 5000);
   }, []);
+
   return (
     <div className="container-fluid w-100 all">
-      
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Tutor data={landingPageData.Tutor} /> 
-      <Team data={landingPageData.Team} />
-      <SideButton/>
+      <AnimatedSection><Header data={landingPageData.Header} /></AnimatedSection>
+      <AnimatedSection><Features data={landingPageData.Features} /></AnimatedSection>
+      <AnimatedSection><About data={landingPageData.About} /></AnimatedSection>
+      <AnimatedSection><Services data={landingPageData.Services} /></AnimatedSection>
+      <AnimatedSection><Tutor data={landingPageData.Tutor} /></AnimatedSection>
+      <AnimatedSection><Team data={landingPageData.Team} /></AnimatedSection>
+      <SideButton />
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
+
