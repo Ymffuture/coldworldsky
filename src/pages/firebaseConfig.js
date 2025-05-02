0b9fd4dc3d059dd0c10b69d6be5e8c1d6fde850f
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-05-02 04:49:00 +0200

    Update firebaseConfig.js

--- src/pages/firebaseConfig.js
+++ src/pages/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAnwHd5fYLC_J46JIJmRrBOt2wBVw6CAbs",
-  authDomain: "visitcount-6309d.firebaseapp.com",
-  projectId: "visitcount-6309d",
-  storageBucket: "visitcount-6309d.firebasestorage.app",
-  messagingSenderId: "662672583632",
-  appId: "1:662672583632:web:5f98b08c04ddb87acba7d5",
-  measurementId: "G-TCG55F765Z"
+  apiKey: visitKey,
+  authDomain: visitauthDomain,
+  projectId: visitproId,
+  storageBucket: visitStoreBucket,
+  messagingSenderId: visitmessaSendId,
+  appId: visitAppId,
+  measurementId: MeasurementIdvisit
 };
 
 // Initialize Firebase
