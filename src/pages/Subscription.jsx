c79e7c8a1678972dea6c7a4d60757faa1222bf1e
ymffuture <futurekgomotso@gmail.com>
2025-05-29 15:49:14 +0200

    20

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -42,7 +42,7 @@ const SubscribePage = () => {
         <Form.Group className="mb-2">
           <Form.Control
             type="email"
-            placeholder="you@example.com"
+            placeholder="yourname@gmail.com"
             {...register('email')}
             className={`form-control ${errors.email ? 'is-invalid' : ''}`}
           />
